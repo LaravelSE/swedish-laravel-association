503c03de155b370580b836d9176477e4cfc57812
Mikko Lauhakari <mikko.lauhakari@glesys.se>
2026-02-10 15:16:15 +0100

    Add Brevo as Mail service.

--- config/services.php
+++ config/services.php
@@ -24,6 +24,10 @@ return [
         'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
     ],
 
+    'brevo' => [
+        'key' => env('BREVO_API_KEY'),
+    ],
+
     'resend' => [
         'key' => env('RESEND_KEY'),
     ],
